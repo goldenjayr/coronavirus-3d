07e72f442412e6b39895867d4d7df783716cddb4
Jay-R Joseph Gabunada <jayrjoseph@dnamicro.com>
2020-05-27 08:46:46 +0800

    Updates

---
+++ precache-manifest.ebeffbf175fc648240f65ee6bec347fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c1243e885dabe1d873b9e48a979f7c",
+    "url": "/goldenjayr/coronavirus-3d.git/index.html"
+  },
+  {
+    "revision": "78edc1db5328561ae7eb",
+    "url": "/goldenjayr/coronavirus-3d.git/static/css/main.6c1f68af.chunk.css"
+  },
+  {
+    "revision": "4ef47e8b7948faa86552",
+    "url": "/goldenjayr/coronavirus-3d.git/static/js/2.4c650b72.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/goldenjayr/coronavirus-3d.git/static/js/2.4c650b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78edc1db5328561ae7eb",
+    "url": "/goldenjayr/coronavirus-3d.git/static/js/main.a54cd8bb.chunk.js"
+  },
+  {
+    "revision": "66b1fc474320251c488d",
+    "url": "/goldenjayr/coronavirus-3d.git/static/js/runtime-main.b6476041.js"
+  },
+  {
+    "revision": "16698fb9638c26fdafd746cefe83c998",
+    "url": "/goldenjayr/coronavirus-3d.git/static/media/Covid-19fin2.16698fb9.glb"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goldenjayr/coronavirus-3d.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
